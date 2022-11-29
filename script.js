83f401394ca6936d091c1612f4498cfeba28fc6a
NickdevC <nick_coles@msn.com>
2022-11-29 20:52:12 +0000

    Explore naming variable conventions

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 console.log(23);
 
-let firstName = "Jonas";
+let firstName = "Matilda";
+
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Constants can be written with capital letters
+let PI = 3.1415;
+
+// Make variable names clear
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
